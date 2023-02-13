8dbf5a85f0514bb6cb6fa5ee3dc667750a14cf6b
Ju San <lucasjukr94@gmail.com>
2023-02-12 21:46:48 -0300

    'code'

---
+++ TextToVoice/TextToSpeech/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace TextToSpeech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize a new instance of the SpeechSynthesizer class
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Choose a voice from the installed voices on your system
+            synth.SelectVoice("Microsoft Zira Desktop");
+
+            // Speak the text
+            synth.Speak("Hello, I'm a text-to-speech voice");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
